1a17e34773163d087705a236daa9e5901be67a98
psp <976477689@qq.com>
2023-03-11 14:58:22 +0800

    修bug

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const ctx = canvas.getContext('2d')
 
 let stepList = []
 let curStepIndex = 0
+let isReDraw = false
 let isMouseDown = false
 window.addEventListener('mousedown', () => {
-    isMouseDown = true
-    stepList.push('newPath')
-    ctx.beginPath()
+    if (!isReDraw) {
+        isMouseDown = true
+        stepList.push('newPath')
+        ctx.beginPath()
+    }
 })
 window.addEventListener('mouseup', () => {
     isMouseDown = false
@@ -34,8 +37,10 @@ window.addEventListener('contextmenu', e => {
 function reDraw() {
     if (curStepIndex === stepList.length) {
         curStepIndex = 0
+        isReDraw = false
         return
     }
+    isReDraw = true
     const curStep = stepList[curStepIndex]
     if (curStep === 'newPath') {
         ctx.beginPath()
